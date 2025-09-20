63843c11f424e53672720cb94eb7d8c76349db43
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-19 23:52:47 -0700

    commit

--- code.cpp
+++ code.cpp
@@ -1,6 +1,26 @@
+// COMSC-210 | Lab 11 | Fahad Fawad Ahmad
+// IDE used: Visual Studio
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <array>
-
 using namespace std;
 
+int main() {
+    //File Opening
+	ifstream file;
+	file.open("elements.txt");
+
+	if (!file.is_open()) {
+	    cout << "Failed to open file" << endl;
+	    return 1;
+	}
+
+    int studentID;
+
+    while (getline(file, studentID)) {
+
+    }
+
+    file.close();
+}
